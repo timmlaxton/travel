2344cc5e807c87618734dedfa04e91833e939400
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-24 14:21:12 +0100

    nodemailer error

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,33 @@ const contactForm = document.querySelector(".contact-form");
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let subject = document.getElementById("subject");
-let nammessagee = document.getElementById("message");
+let message = document.getElementById("message");
 
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Email sent");
+      name.value = "";
+      email.value = "";
+      subject.value = "";
+      message.value = "";
+    } else {
+      alert("An error has occured");
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
 });
--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 const express = require("express");
 const app = express();
 
+const nodemailer = require("nodemailer");
+
 const PORT = process.env.PORT || 5000;
 
 // MiddleWare
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "audioeverything100@gmail.com",
+      password: "Monkey01!",
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "audioeverything100@gmail.com",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent " + info.response);
+      res.send("success");
+    }
+  });
 });
 
 app.listen(PORT, () => {
