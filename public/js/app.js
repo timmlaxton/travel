cea4c3f214895c75bb617c2ab4f81e00da1c77df
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-24 12:49:42 +0100

    added selectors

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const contactForm = document.querySelector(".contact-form");
+
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let nammessagee = document.getElementById("message");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
